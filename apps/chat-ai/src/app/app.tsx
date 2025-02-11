844182b21ee8cb75e19097da360ee635eb40ccf7
Nnder <asfahaikrkt6k56@yandex.ru>
2025-02-11 08:43:12 +0300

    Вывод ошибки, загрузка

--- apps/chat-ai/src/app/app.tsx
+++ apps/chat-ai/src/app/app.tsx
@@ -2,18 +2,21 @@ import { useState } from 'react';
 import { MainLayout } from './components/layout';
 import { LoaderCircle } from 'lucide-react';
 
+type QuestionStatus = 'error' | 'loading' | 'loaded';
 interface IQuestion {
   message: string;
   answer: string;
+  status: QuestionStatus;
+  errorMessage?: string;
 }
 
 export function App() {
   const [question, setQuestion] = useState(
-    'в чем смысл жизни, напиши минимум 20 предложений'
+    'в чем смысл жизни, напиши минимум 4 предложения'
   );
 
-  const [items, setItems] = useState<IQuestion[]>([]);
   const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState<IQuestion[]>([]);
 
   const setData = async (
     items: IQuestion[],
@@ -30,58 +33,69 @@ export function App() {
         const reader = response.body.getReader();
         const decoder = new TextDecoder();
 
-        // Функция для чтения порций данных
         function read() {
+          const d = [...items];
           reader
             .read()
             .then(({ done, value }) => {
               if (done) {
-                console.log('Поток завершён');
+                d[index].status = 'loaded';
+                console.log('Поток завершён', d[index].status);
+                setItems(d);
                 setLoading(false);
                 return;
               }
-              // Декодируем полученный фрагмент и выводим его в консоль
               const chunk = decoder.decode(value, { stream: true });
-              const d = [...items];
-              console.log(index);
-              console.log(d);
               d[index].answer += chunk;
               setItems(d);
-              console.log('Полученные данные:', chunk);
-              // Рекурсивно считываем следующий фрагмент
               read();
             })
             .catch((error) => {
-              console.error('Ошибка при чтении потока:', error);
+              d[index].status = 'error';
+              d[index].errorMessage = error.message;
+              setLoading(false);
             });
         }
 
         read();
       })
       .catch((error) => {
-        console.error('Ошибка запроса:', error);
+        items[index].status = 'error';
+        items[index].errorMessage = error.message;
+        setItems(items);
+        setLoading(false);
       });
   };
 
   const Send = async () => {
+    setLoading(true);
     setItems((prev) => {
-      const newItems = [...prev, { message: question, answer: '' }];
+      const newItems = [
+        ...prev,
+        { message: question, answer: '', status: 'loading' as QuestionStatus },
+      ];
       setData(newItems, newItems.length - 1, question);
       return newItems;
     });
-
     setQuestion('');
-    setLoading(true);
   };
 
   return (
     <MainLayout>
       <div className="shadow-2xl p-4 flex flex-col justify-between w-full lg:w-2/3 bg-slate-300 text-slate-800 rounded-xl max-h-[calc(100vh-120px)] min-h-[calc(100vh-120px)]">
-        <div className="h-full max-h-full overflow-y-auto">
+        <div className="h-full max-h-full overflow-y-auto pr-3">
           {items.map((item, index) => (
-            <div key={index}>
+            <div key={index} className="h-full flex flex-col overflow-hidden">
               <div className="text-right p-2">{item.message}</div>
               <div className="text-left p-2">{item.answer}</div>
+              {item.status === 'loading' && (
+                <div>
+                  <LoaderCircle size={48} className="animate-spin" />
+                </div>
+              )}
+              {item.status === 'error' && (
+                <div>{item?.errorMessage ?? 'Ошибка'}</div>
+              )}
             </div>
           ))}
         </div>
@@ -94,15 +108,15 @@ export function App() {
           ></textarea>
           <button
             className="bg-slate-500 p-4 text-white font-bold rounded-xl w-full md:w-36 h-20"
-            disabled={loading}
             onClick={Send}
+            disabled={loading}
           >
             {loading ? (
-              <div className="flex justify-center ">
+              <div className="flex justify-center">
                 <LoaderCircle size={48} className="animate-spin" />
               </div>
             ) : (
-              'Отправить'
+              ' Отправить'
             )}
           </button>
         </div>
