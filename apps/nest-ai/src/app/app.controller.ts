5259b961b92bfff54c7019e65cd05e75000bacc0
Nnder <asfahaikrkt6k56@yandex.ru>
2025-02-11 08:48:00 +0300

    backend готов

--- apps/nest-ai/src/app/app.controller.ts
+++ apps/nest-ai/src/app/app.controller.ts
@@ -1,46 +1,19 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import OpenAI from 'openai';
 import { Response } from 'express';
 
 @Controller('chat')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
-
   @Post('question')
   async newQuestion(@Res() res: Response, @Body() body) {
-    console.log(body.question)
-    const openai = new OpenAI({
-      baseURL: "https://openrouter.ai/api/v1",
-      apiKey: process.env.DEEPSEEK_API_KEY ?? '',
-    })
-
     res.setHeader('Content-Type', 'text/plain');
-
-      const completion = await openai.chat.completions.create({
-        model: "qwen/qwen-vl-plus:free",
-        // model: "meta-llama/llama-3.2-3b-instruct:free",
-        messages: [
-          {
-            "role": "user",
-            "content": body.question
-          }
-        ],
-        stream: true,
-      })
-
+    const completion = await this.appService.newQuestion(body.question)
       for await (const chunk of completion) {
         const content = chunk.choices[0]?.delta?.content || '';
-        console.log(content)
         res.write(content);
       }
-
-      res.end();
-
+    res.end();
   }
 }
--- apps/nest-ai/src/app/app.service.ts
+++ apps/nest-ai/src/app/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async newQuestion(question: string) {
+    const openai = new OpenAI({
+      baseURL: process.env.BASE_URL ?? '',
+      apiKey: process.env.OPENAI_API_KEY ?? '',
+    })
+    const completion = await openai.chat.completions.create({
+      model: process.env.MODEL,
+      messages: [
+        {
+          "role": "user",
+          "content": question
+        }
+      ],
+      stream: true,
+    })
+
+    return completion
   }
 }
