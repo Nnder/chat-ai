962407424139693542367028bc5d308f6aed839b
Nnder <asfahaikrkt6k56@yandex.ru>
2025-02-11 09:16:21 +0300

    Добавил swagger и изменил тесты

--- apps/chat-ai/src/app/app.spec.tsx
+++ apps/chat-ai/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(new RegExp('Welcome chat-ai', 'gi'))).toBeTruthy();
+    expect(getByText(new RegExp('Чат с AI', 'gi'))).toBeTruthy();
   });
 });
--- apps/nest-ai/src/main.ts
+++ apps/nest-ai/src/main.ts
@@ -6,10 +6,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+  .setTitle('AI example')
+  .setDescription('The AI API description')
+  .setVersion('1.0')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
